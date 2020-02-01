7953bd4e59ac30620d486d992342a329478671c4
Justin Chen <chenz249@uw.edu>
2020-01-31 21:04:09 -0800

    Exercise week4

--- chapter-11-exercises/exercise-4/exercise.R
+++ chapter-11-exercises/exercise-4/exercise.R
@@ -2,41 +2,48 @@
 
 # Install the `"nycflights13"` package. Load (`library()`) the package.
 # You'll also need to load `dplyr`
-
+install.packages("nycflights13")
+library(nycflights13)
+library("dplyr")
 
 # The data frame `flights` should now be accessible to you.
 # Use functions to inspect it: how many rows and columns does it have?
 # What are the names of the columns?
 # Use `??flights` to search for documentation on the data set (for what the 
 # columns represent)
-
+dim(flights) #336776 rows, 20 columns
+colnames(flights) 
 
 # Use `dplyr` to give the data frame a new column that is the amount of time
 # gained or lost while flying (that is: how much of the delay arriving occured
 # during flight, as opposed to before departing).
-
+flights <- mutate(flights, 
+                  delayed_in_air = arr_delay - dep_delay)
 
 # Use `dplyr` to sort your data frame in descending order by the column you just
 # created. Remember to save this as a variable (or in the same one!)
-
+descending_order <- arrange(flights, -delayed_in_air)
 
 # For practice, repeat the last 2 steps in a single statement using the pipe
 # operator. You can clear your environmental variables to "reset" the data frame
-
+sorted_flights <- flights %>% 
+  mutate(dep_delay + arr_delay) %>% 
+  arrange(-(dep_delay + arr_delay))
 
 # Make a histogram of the amount of time gained using the `hist()` function
 
 
 # On average, did flights gain or lose time?
 # Note: use the `na.rm = TRUE` argument to remove NA values from your aggregation
-
+summarize(flights, avg = mean(delayed_in_air, na.rm = TRUE))
 
 # Create a data.frame of flights headed to SeaTac ('SEA'), only including the
 # origin, destination, and the "gain_in_air" column you just created
-
+sea <- flights %>%
+  filter(dest == "SEA")
 
 # On average, did flights to SeaTac gain or loose time?
-
+summarize(sea, avg_delayed = mean(delayed_in_air, na.rm = TRUE))
 
 # Consider flights from JFK to SEA. What was the average, min, and max air time
 # of those flights? Bonus: use pipes to answer this question in one statement
